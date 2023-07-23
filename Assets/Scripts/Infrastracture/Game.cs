204b227f4b400bf27528b4717f6c8c558e3193a8
Mikhail Turlyakov <reelkek@gmail.com>
2023-07-23 13:33:34 +0400

    add anime characters asset

--- Assets/Scripts/Infrastracture/Game.cs
+++ Assets/Scripts/Infrastracture/Game.cs
@@ -1,6 +1,26 @@
-﻿namespace Infrastracture
+﻿using Services.Input;
+using Unity.VisualScripting.Dependencies.Sqlite;
+using UnityEngine.Device;
+
+namespace Infrastracture
 {
     public class Game
     {
+        public static InputService InputService;
+
+        public Game()
+        {
+            InitializeInputService();
+        }
+
+        private static void InitializeInputService()
+        {
+            if (Application.isEditor)
+                InputService = new DesktopInputService();
+            else
+            {
+                InputService = new MobileInputService();
+            }
+        }
     }
 }
---
+++ Assets/Scripts/Services/Input/InputService.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace Services.Input
+{
+    public abstract class InputService : IInputService
+    {
+        protected const string Horizontal = "Horizontal";
+        protected const string Vertical = "Vertical";
+        private const string Attack = "Attack";
+
+        public abstract Vector2 Axis { get; }
+
+        public bool IsAttackButtonClicked()
+        {
+            return SimpleInput.GetButtonUp(Attack);
+        }
+
+        protected static Vector2 ReceiveSimpleInputAxis() =>
+            new(SimpleInput.GetAxis(Horizontal), SimpleInput.GetAxis(Vertical));
+    }
+
+    class MobileInputService : InputService
+    {
+        public override Vector2 Axis { get; }
+    }
+    
+    class DesktopInputService : InputService
+    {
+        public override Vector2 Axis
+        {
+            get
+            {
+                Vector2 axis = ReceiveSimpleInputAxis();
+                if (axis == Vector2.zero)
+                    axis = ReceiveUnityInputAxis();
+                return axis;
+            }
+        }
+
+        private static Vector2 ReceiveUnityInputAxis() => new(UnityEngine.Input.GetAxis(Horizontal),
+            UnityEngine.Input.GetAxis(Vertical));
+    }
+}
