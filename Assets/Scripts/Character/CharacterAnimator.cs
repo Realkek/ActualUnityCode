76c97bdd2eb41fc077177e15ac1063d48967c81f
Mikhail Turlyakov <reelkek@gmail.com>
2023-07-24 15:37:47 +0400

    enable run animation

--- Assets/Scripts/Character/CharacterAnimator.cs
+++ Assets/Scripts/Character/CharacterAnimator.cs
@@ -8,6 +8,7 @@ namespace Character
     public class CharacterAnimator : MonoBehaviour
     {
         private static readonly int RunningHash = Animator.StringToHash("Running");
+        private static readonly int IdleHash = Animator.StringToHash("Idle");
         private readonly int _walkingStateHash = Animator.StringToHash("Run");
         private InputService _inputService;
         public Animator Animator;
@@ -21,11 +22,16 @@ namespace Character
         private void Update()
         {
             if (_inputService.Axis.sqrMagnitude > Constants.Epsilon)
-                Animator.SetFloat(RunningHash, CharacterController.velocity.magnitude, 0.1f, Time.deltaTime);
+            {
+                Animator.SetFloat(RunningHash, CharacterController.velocity.magnitude, 0.01f, Time.deltaTime);
+                Animator.SetBool(IdleHash, false);
+            }
             else
             {
-                Animator.SetFloat(RunningHash, 0f, 0.1f, Time.deltaTime);
+                Animator.SetFloat(RunningHash, 0);
+                Animator.SetBool(IdleHash, true);
             }
         }
+        
     }
 }
--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -2,5 +2,5 @@ using UnityEngine;
 
 public static class Constants
 {
-  public const float Epsilon = 0.001f; 
+  public const float Epsilon = 0.01f; 
 }
